4f91e4ec5513fec55f25a3b2bf813eb767a31255
AnadeCordoba <108664842+AnadeCordoba@users.noreply.github.com>
2022-07-10 12:28:05 +0200

    muevo la foto a la carpeta prueba

--- servidor.py
+++ servidor.py
@@ -4,7 +4,17 @@ app = Flask(__name__)
 
 @app.route('/')
 def hello_world():
-    return 'Hello, World!'
+    return '''
+    <!DOCTYPE html>
+<html>
+  <head>
+    <meta charset="utf-8">
+    <title>Mi pagina de prueba</title>
+  </head>
+  <body>
+    <img src="DSC_0601.jpg" alt="Mi imagen de prueba">
+  </body>
+</html> '''
 
 if __name__ == '__main__':
     app.run()
